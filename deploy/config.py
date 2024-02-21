8e634ec9315fa6b4827f89e0d99f0b790056e7b7
ahada-wg <155437555+ahada-wg@users.noreply.github.com>
2024-02-21 11:47:30 +0530

    added config file in deploy

---
+++ deploy/config.py
@@ -0,0 +1,37 @@
+import os
+import dotenv
+
+from deploy.run_helper import get_stack_qualifier
+
+
+dotenv.load_dotenv()
+ 
+ADMIN_EMAIL = [os.getenv("ADMIN_EMAIL")]
+ENVIRONMENT = os.getenv("ENVIRONMENT")
+S3_BUCKET_FOR_SAM = os.getenv("BUCKET_NAME")
+JWT_SECRET = os.getenv("JWT_SECRET")
+
+SAM_STACK_NAME = f"{get_stack_qualifier('api-gateway')}"
+BUCKET_NAME = f"{get_stack_qualifier('static-content')}"
+USER_POOL_NAME = f"{get_stack_qualifier('user-pool')}"
+USER_POOL_CLIENT_NAME = f"{get_stack_qualifier('user-pool-client')}"
+TABLE_NAME = f"{get_stack_qualifier('database')}"
+ROLE_NAME = f"{get_stack_qualifier('lambda-role-common')}"
+SENDER_EMAIL = ADMIN_EMAIL
+RECIPIENT_EMAIL = ADMIN_EMAIL
+
+
+configurations = {
+    "ADMIN_EMAIL": [ADMIN_EMAIL],
+    "ENVIRONMENT": ENVIRONMENT,
+    "S3_BUCKET_FOR_SAM": BUCKET_NAME,
+    "JWT_SECRET": JWT_SECRET,
+    "SAM_STACK_NAME": f"{get_stack_qualifier('api-gateway')}",
+    "BUCKET_NAME": f"{get_stack_qualifier('static-content')}",
+    "USER_POOL_NAME": f"{get_stack_qualifier('user-pool')}",
+    "USER_POOL_CLIENT_NAME": f"{get_stack_qualifier('user-pool-client')}",
+    "TABLE_NAME": f"{get_stack_qualifier('database')}",
+    "ROLE_NAME": f"{get_stack_qualifier('lambda-role-common')}",
+    "SENDER_EMAIL": ADMIN_EMAIL,
+    "RECIPIENT_EMAIL": ADMIN_EMAIL,
+}
--- deploy/run.py
+++ deploy/run.py
@@ -1,46 +1,31 @@
-import os
 import subprocess
 import boto3
 import logging
-import dotenv
- 
+
+import config 
 from run_helper import create_recursive_folders, get_stack_qualifier
 from ses_template import deploy_template, send_verification_mails, body_mail
- 
-dotenv.load_dotenv()
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
- 
 client_cf = boto3.client("cloudformation")
-outputs = {}
-ADMIN_EMAIL = os.getenv("ADMIN_EMAIL")
-configurations = {
-    "ADMIN_EMAIL": [ADMIN_EMAIL],
-    "ENVIRONMENT": os.getenv("ENVIRONMENT"),
-    "S3_BUCKET_FOR_SAM": os.getenv("BUCKET_NAME"),
-    "SAM_STACK_NAME": f"{get_stack_qualifier('api-gateway')}",
-    "BUCKET_NAME": f"{get_stack_qualifier('static-content')}",
-    "USER_POOL_NAME": f"{get_stack_qualifier('user-pool')}",
-    "USER_POOL_CLIENT_NAME": f"{get_stack_qualifier('user-pool-client')}",
-    "TABLE_NAME": f"{get_stack_qualifier('database')}",
-    "ROLE_NAME": f"{get_stack_qualifier('lambda-role-common')}",
-    "SENDER_EMAIL": ADMIN_EMAIL,
-    "RECIPIENT_EMAIL": ADMIN_EMAIL,
-    "JWT_SECRET": os.getenv("JWT_SECRET"),
-}
+
  
-def get_iam_stack(outputs, configurations=configurations):
+outputs = {}
+
+ 
+def get_iam_stack(outputs, ):
     iam_stack = {
         "stack_name": "iam-policy",
         "template_body_url": "cfn/iam_policy.yaml",
         "parameters": [
             {
                 "ParameterKey": "RoleName",
-                "ParameterValue": configurations.get("ROLE_NAME"),
+                "ParameterValue": config.ROLE_NAME,
             },
             {
                 "ParameterKey": "Environment",
-                "ParameterValue": configurations.get("ENVIRONMENT"),
+                "ParameterValue": config.ENVIRONMENT,
             },
             {"ParameterKey": "BucketArn", "ParameterValue": outputs["BucketArn"]},
             {
@@ -59,7 +44,7 @@ static_content_bucket_stack = {
     "parameters": [
         {
             "ParameterKey": "BucketName",
-            "ParameterValue": configurations.get("BUCKET_NAME"),
+            "ParameterValue": config.BUCKET_NAME,
         },
     ],
     "capabilities": ["CAPABILITY_IAM"],
@@ -70,15 +55,15 @@ cognito_stack = {
     "parameters": [
         {
             "ParameterKey": "Environment",
-            "ParameterValue": configurations.get("ENVIRONMENT"),
+            "ParameterValue": config.ENVIRONMENT,
         },
         {
             "ParameterKey": "UserPoolName",
-            "ParameterValue": configurations.get("USER_POOL_NAME"),
+            "ParameterValue": config.USER_POOL_NAME,
         },
         {
             "ParameterKey": "UserPoolClientName",
-            "ParameterValue": configurations.get("USER_POOL_CLIENT_NAME"),
+            "ParameterValue": config.USER_POOL_CLIENT_NAME,
         },
     ],
     "capabilities": ["CAPABILITY_IAM"],
@@ -89,11 +74,11 @@ dynamodb_stack = {
     "parameters": [
         {
             "ParameterKey": "Environment",
-            "ParameterValue": configurations.get("ENVIRONMENT"),
+            "ParameterValue": config.ENVIRONMENT,
         },
         {
             "ParameterKey": "TableName",
-            "ParameterValue": configurations.get("TABLE_NAME"),
+            "ParameterValue": config.TABLE_NAME,
         },
     ],
     "capabilities": ["CAPABILITY_IAM"],
@@ -161,7 +146,7 @@ def run_command(command):
 def apigateway_lambda_deploy_sam():
     package_command = (
         "sam package "
-        f"--s3-bucket {configurations.get('S3_BUCKET_FOR_SAM')} "
+        f"--s3-bucket {config.S3_BUCKET_FOR_SAM} "
         "--template-file template.yaml "
         "--output-template-file template-generated.yaml"
     )
@@ -171,18 +156,18 @@ def apigateway_lambda_deploy_sam():
     deploy_command = (
         "sam deploy "
         "--template-file template-generated.yaml "
-        f"--stack-name {configurations.get('SAM_STACK_NAME')} "
+        f"--stack-name {config.SAM_STACK_NAME} "
         "--capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM "
-        f"--parameter-overrides Environment={configurations.get('ENVIRONMENT')} "
+        f"--parameter-overrides Environment={config.ENVIRONMENT} "
         f"BucketName={outputs.get('BucketName')} "
         f"DynamoDBTable={outputs.get('DynamoDBTableName')} "
         f"UserPoolId={outputs.get('UserPoolId')} "
         f"UserPoolClientId={outputs.get('UserPoolClientId')} "
         f"LambdaIAMRoleArn={outputs.get('LambdaIAMRoleArn')} "
         f"CognitoPoolArn={outputs.get('CognitoPoolArn')} "
-        f"SenderMail={configurations.get('SENDER_EMAIL')} "
-        f"ReceiverMail={configurations.get('RECIPIENT_EMAIL')} "
-        f"JWTSecret={configurations.get('JWT_SECRET')}"
+        f"SenderMail={config.SENDER_EMAIL} "
+        f"ReceiverMail={config.RECIPIENT_EMAIL} "
+        f"JWTSecret={config.JWT_SECRET}"
     )
     logger.info("Deploying SAM application...")
     run_command(deploy_command)
@@ -202,11 +187,11 @@ def main():
     deploy_stack(**static_content_bucket_stack)
     deploy_stack(**cognito_stack)
     deploy_stack(**dynamodb_stack)
-    send_verification_mails(configurations.get("ADMIN_EMAIL"))
+    send_verification_mails(config.ADMIN_EMAIL)
     deploy_stack(**get_iam_stack(outputs))
     install_requirements()
     apigateway_lambda_deploy_sam()
-    get_stack_outputs(configurations.get("SAM_STACK_NAME"))
+    get_stack_outputs(config.SAM_STACK_NAME)
     APIGatewayEndpoint = outputs.get("VMSApiEndpoint")
     deploy_template(
         "vms_email_template-test",
